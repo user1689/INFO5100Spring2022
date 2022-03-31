4ab3615e9b2d8049ad04c6218b7c2a00fbfeb425
sb <89725525+user1689@users.noreply.github.com>
2022-03-31 14:52:53 -0700

    update assignment4 31 March

--- assignment4/src/testQueue.java
+++ assignment4/src/testQueue.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class testQueue {
 
     public static void main(String[] args) throws Exception{
-        Queue q = new Queue(4);
+        Queue<Integer> q = new Queue<Integer>(4);
 
         int threads = 2;
         int times = 10;
@@ -46,5 +46,46 @@ public class testQueue {
         for (Thread thread : threadList) {
             thread.join();
         }
+
+        // test whether push and pop of thread is safe
+        if (q.isEmpty()) {
+            System.out.println("queue is empty");
+        } else {
+            System.out.println("queue is not empty");
+        }
+
+        // test whether peek of thread is safe
+        Thread consumer2 = new Thread(() -> {
+            try {
+                int x = q.peek();
+                System.out.println(x);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread producer2 = new Thread(() -> {
+            try {
+                q.add(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread consumer3 = new Thread(() -> {
+            try {
+                q.remove();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        consumer2.start();
+        consumer3.start();
+        Thread.sleep(2000);
+        producer2.start();
+        consumer2.join();
+        producer2.join();
+        consumer3.join();
     }
 }
